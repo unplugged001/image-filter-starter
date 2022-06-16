6baa19acd725bffc2842763b9dcdf84f68ec9ef5
unplugged001 <ochuwilliamsirawo@gmail.com>
2022-06-16 16:14:34 +0100

    Working app deployment

---
+++ image-filter-starter-code/YOU_MUST_README.txt
@@ -0,0 +1,22 @@
+Hello 
+
+I am Ochu Williams Faithful and this is a list of project submission details
+
+N:B: The wikepedia links always return an error when filtered. However, other links are working well enough. 
+
+1. Elasticbeanstalk url 
+
+image-starter-filter-dev-dev.us-east-1.elasticbeanstalk.com
+
+2. Filtered image url 
+
+http://image-starter-filter-dev-dev.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://phantom-marca.unidadeditorial.es/daf82cf51df827d3078488f109340eab/crop/0x23/2047x1174/resize/1200/f/jpg/assets/multimedia/imagenes/2022/01/17/16424134696917.jpg
+
+3. Image of Elasticbeanstalk healthy app environment 
+
+
+
+
+
+
+
---
+++ image-filter-starter-code/src/server.ts
@@ -0,0 +1,66 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+// RESTFUL_API ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  app.get("/filteredimage", async (req, res) => {
+    // validate the image_url query
+      const{image_url}= req.query
+      if(!image_url){
+        return res.status(400).send("image_url is missing")
+      }
+      try {
+    // call filterImageFromURL(image_url) to filter the image and https://sourceforge.net/projects/unxutils/   
+        const imagePath= await filterImageFromURL(image_url)
+        return res.sendFile(imagePath, async (error)=>{
+          await deleteLocalFiles ([imagePath])
+        });
+          } catch (error) {
+    // send the resulting file in the response        
+        return res.status(422).send("image url cannot be processed")
+      }
+    } );
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
